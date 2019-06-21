c24c08b5d678cbbe7f76811208b6589e1cb83a59
Euheny <Jeka_janovich@mail.ru>
2019-06-21 19:23:05 +0300

    Home work Занятие_6

---
+++ Занятие_6/Gradle/src/main/java/Lesson_6_Gradle/List.java
@@ -0,0 +1,56 @@
+package Lesson_6_Gradle;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class List {
+    public ArrayList<Integer> getFilledList(int listSize) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < listSize; i++) {
+            list.add( (int) (Math.random() * 100) );
+        }
+        return list;
+    }
+
+    public int getMaxValueList(ArrayList<Integer> list) {
+        return Collections.max( list );
+    }
+
+    public int getMinValueList(ArrayList<Integer> list) {
+        return Collections.min( list );
+    }
+
+    public ArrayList<Integer> getSortList(ArrayList<Integer> list) {
+        Collections.sort( list );
+        return list;
+    }
+
+    public ArrayList<Integer> getListReplaceValue(ArrayList<Integer> list, int value, int newValue) {
+        Collections.replaceAll( list, value, newValue );
+        return list;
+    }
+
+    public boolean getListIsEmpty(ArrayList<Integer> list) {
+        return list.isEmpty();
+    }
+
+    public ArrayList<Integer> getReverseList(ArrayList<Integer> list) {
+        Collections.reverse( list );
+        return list;
+    }
+
+    public ArrayList<Integer> getListChangeValueByIndex(ArrayList<Integer> list, int i, int j) {
+        Collections.swap( list, i, j );
+        return list;
+    }
+
+    public ArrayList<String> getListRemove(ArrayList<String> listFirst, ArrayList<String> listSecond) {
+        listFirst.removeAll( listSecond );
+        return listFirst;
+    }
+
+    public ArrayList<String> getListRetein(ArrayList<String> listFirst, ArrayList<String> listSecond) {
+        listFirst.retainAll( listSecond );
+        return listFirst;
+    }
+}
---
+++ Занятие_6/Gradle/src/test/java/Lesson_6_Gradle/ListTest.java
@@ -0,0 +1,88 @@
+package Lesson_6_Gradle;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ListTest {
+    List list = new List();
+
+    @Test
+    void getFilledListTest() {
+        int sizeList = 15;
+        ArrayList<Integer> arrList = list.getFilledList( sizeList );
+        Assertions.assertNotNull( arrList );
+        Assertions.assertEquals( arrList.size(), sizeList );
+    }
+
+    @Test
+    void getMaxValueListTest() {
+        ArrayList<Integer> arlist = new ArrayList( Arrays.asList( 1, 2, 3, 5, 8, 13, 21 ) );
+        int expected = 21;
+        Assertions.assertEquals( expected, list.getMaxValueList( arlist ) );
+    }
+
+    @Test
+    void getMinValueListTest() {
+        ArrayList<Integer> arlist = new ArrayList( Arrays.asList( 1, 2, 3, 5, 8, 13, 21 ) );
+        int expected = 1;
+        Assertions.assertEquals( expected, list.getMinValueList( arlist ) );
+    }
+
+    @Test
+    void getSortListTest() {
+        ArrayList<Integer> arlist = new ArrayList( Arrays.asList( 23, 54, 12, 87, 96, 33, 21 ) );
+        ArrayList<Integer> expected = new ArrayList( Arrays.asList( 12, 21, 23, 33, 54, 87, 96 ) );
+        Assertions.assertEquals( expected, list.getSortList( arlist ) );
+    }
+
+    @Test
+    void getListReplaceValueTest() {
+        ArrayList<Integer> arlist = new ArrayList( Arrays.asList( 23, 54, 12, 87, 54, 33, 54 ) );
+        ArrayList<Integer> expected = new ArrayList( Arrays.asList( 23, 5, 12, 87, 5, 33, 5 ) );
+        int value = 54;
+        int replaceValue = 5;
+        Assertions.assertEquals( expected, list.getListReplaceValue( arlist, value, replaceValue ) );
+    }
+
+    @Test
+    void getListIsEmptyTest() {
+        ArrayList<Integer> arlist = new ArrayList<Integer>(Arrays.asList( 23, 54, 12, 87, 96, 33, 21 ));
+        Assertions.assertTrue( list.getListIsEmpty( arlist ) );
+    }
+
+    @Test
+    void getReverseListTest() {
+        ArrayList<Integer> arlist = new ArrayList( Arrays.asList( 23, 54, 12, 87, 96, 33, 21 ) );
+        ArrayList<Integer> expected = new ArrayList( Arrays.asList( 21, 33, 96, 87, 12, 54, 23 ) );
+        Assertions.assertEquals( expected, list.getReverseList( arlist ) );
+    }
+
+    @Test
+    void getListChangeValueByIndexTest() {
+        ArrayList<Integer> arlist = new ArrayList( Arrays.asList( 23, 54, 12, 87, 96, 33, 21 ) );
+        ArrayList<Integer> expected = new ArrayList( Arrays.asList( 23, 33, 12, 87, 96, 54, 21 ) );
+        int index = 1;
+        int changeIndex = 5;
+        Assertions.assertEquals( expected, list.getListChangeValueByIndex( arlist, index, changeIndex ) );
+    }
+
+    @Test
+    void getListRemoveTest() {
+        ArrayList<String> arlistFirst = new ArrayList( Arrays.asList( "White", "Black", "Red" ) );
+        ArrayList<String> arlistSecond = new ArrayList( Arrays.asList( "Green", "Red", "White" ) );
+        ArrayList<String> expected = new ArrayList( Arrays.asList( "Black" ) );
+        Assertions.assertEquals( expected, list.getListRemove( arlistFirst, arlistSecond ) );
+    }
+
+    @Test
+    void getListClearTestc() {
+        ArrayList<String> arlistFirst = new ArrayList( Arrays.asList( "White", "Black", "Red" ) );
+        ArrayList<String> arlistSecond = new ArrayList( Arrays.asList( "Green", "Red", "White" ) );
+        ArrayList<String> expected = new ArrayList( Arrays.asList( "White", "Red" ) );
+        Assertions.assertEquals( expected, list.getListRetein( arlistFirst, arlistSecond ) );
+    }
+
+}
